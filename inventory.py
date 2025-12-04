eafbb2aa90fbf72890b494896b2cbe76e3ce4c16
veereshsajjansajjan1-svg <veereshsajjansajjan1@gmail.com>
2025-12-04 17:38:58 +0530

    Change argument error message to use user values
    Updated the message for missing arguments to indicate user values will be used instead of defaults.

--- inventory.py
+++ inventory.py
@@ -1,24 +1,19 @@
 import sys
 
-default_product = "UnknownProduct"
-default_quantity = 0
-default_brand = "NoBrand"
-default_price = 0.0
-
-
 if len(sys.argv) != 5:
-    print("Arguments not provided correctly. Using default values...\n")
-    
-    product_name = default_product
-    quantity = default_quantity
-    brand = default_brand
-    price = default_price
+    print("Arguments not provided correctly. Using User values...\n")
 
-else:
     product_name = sys.argv[1]
     quantity = int(sys.argv[2])
     brand = sys.argv[3]
     price = float(sys.argv[4])
+
+else:
+
+ default_product = "UnknownProduct"
+ default_quantity = 0
+ default_brand = "NoBrand"
+ default_price = 0.0
 
 print("\n -- Inventory Details --")
 print(f"Product Name: {product_name}")
