033097887f1f5f57b8baa628c4841df372693e37
veeresh <veeresh@123>
2025-12-04 17:15:30 +0530

    save

--- inventory.py
+++ inventory.py
@@ -1,23 +1,21 @@
 import sys
 
-if len(sys.argv) !=5:
-
+if len(sys.argv) != 5:
+    print("Usage: python inventory.py <product_name> <quantity> <brand> <price>")   
     sys.exit()
 
-    product_name = sys.argv[1]
-    quantity = int(sys.argv[2])
-    brand = sys.argv[3]
-    price = float(sys.argv[4])
+product_name = sys.argv[1]
+quantity = int(sys.argv[2])
+brand = sys.argv[3]
+price = float(sys.argv[4])
 
-    print("\n -- Inventory Details --")
-    print(f"Product Name: {product_name}")
-    print(f"Quantity: {quantity}")      
-    print(f"Brand: {brand}")
-    print(f"Price: ${price:.2f}")
+print("\n -- Inventory Details --")
+print(f"Product Name: {product_name}")
+print(f"Quantity: {quantity}")
+print(f"Brand: {brand}")
+print(f"Price: ${price:.2f}")
 
-    if quantity > 50:
-        print("stock status: high stock available")
-    else :
-        print("stock status: low stock available")
-
-        
+if quantity > 50:
+    print("Stock Status: High stock available")
+else:
+    print("Stock Status: Low stock available")
